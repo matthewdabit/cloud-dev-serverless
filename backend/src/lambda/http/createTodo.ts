539be7b523274e29a741fac8ac9262219bd4cc21
Matthew Dabit <matthew.dabit@numerator.com>
2020-05-03 23:01:28 -0500

    Add gitignore. Change CreateTodo

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,14 +1,14 @@
 import 'source-map-support/register';
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import {APIGatewayProxyEvent, APIGatewayProxyResult} from 'aws-lambda';
 
-import { CreateTodoRequest } from '../../requests/CreateTodoRequest';
-import * as AWS  from 'aws-sdk';
+import {CreateTodoRequest} from '../../requests/CreateTodoRequest';
+import * as AWS from 'aws-sdk';
 import * as uuid from 'uuid';
 import * as middy from 'middy';
-import { cors } from 'middy/middlewares';
+import {cors} from 'middy/middlewares';
 
-import { parseUserId } from "../../auth/utils";
+import {parseUserId} from "../../auth/utils";
 
 const todosTable = process.env.TODOS_TABLE;
 const docClient = new AWS.DynamoDB.DocumentClient();
@@ -16,47 +16,33 @@ const docClient = new AWS.DynamoDB.DocumentClient();
 
 export const handler = middy(
     async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-      const parsedBody: CreateTodoRequest = JSON.parse(event.body);
+          const parsedBody: CreateTodoRequest = JSON.parse(event.body);
+          const authorization = event.headers.Authorization;
+          const split = authorization.split(' ');
+          const jwtToken = split[1];
+          const newTodo = {
+                id: uuid.v4(),
+                userId: parseUserId(jwtToken),
+                createdAt: new Date().toISOString(),
+                done: false,
+                ...parsedBody
+          };
 
-      const todoId = uuid.v4();
-      const authorization = event.headers.Authorization;
-      const split = authorization.split(' ');
-      const jwtToken = split[1];
-      const createdAt = new Date().toISOString();
-      const newTodo = {
-        id: todoId,
-        userId: parseUserId(jwtToken),
-        createdAt: createdAt,
-        done: false,
-        ...parsedBody
-      };
-
-      try {
         await docClient.put({
-          TableName: todosTable,
-          Item: newTodo
+              TableName: todosTable,
+              Item: newTodo
         }).promise();
 
         return {
-          statusCode: 201,
-          headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Credentials': true
-          },
-          body: JSON.stringify({
-            newTodo
-          })
+              statusCode: 201,
+              headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true
+              },
+              body: JSON.stringify({
+                newTodo
+              })
         };
-      } catch (error) {
-        return {
-          statusCode: 404,
-          headers: {
-            'Access-Control-Allow-Origin': '*'
-          },
-          body: ''
-        }
-      }
-
 
     }
 );
